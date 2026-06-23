58948630fadbcc04ebce9b305e2e923d74c7c5d9
ltvhcung001 <ltv.h.cung001@gmail.com>
2026-06-23 11:36:08 +0700

    Done HW2

---
+++ patient-service/src/main/java/com/rikkeiedu/patientservice/controller/PatientController.java
@@ -0,0 +1,22 @@
+package com.rikkeiedu.patientservice.controller;
+
+import com.rikkeiedu.patientservice.entity.Patient;
+import com.rikkeiedu.patientservice.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @PostMapping
+    public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+        Patient savedPatient = patientRepository.save(patient);
+        return new ResponseEntity<>(savedPatient, HttpStatus.CREATED);
+    }
+}
