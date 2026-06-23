535e3a1207ecd5c07286de61ceaabd02cba48f38
ltvhcung001 <ltv.h.cung001@gmail.com>
2026-06-23 11:44:52 +0700

    Done HW3

---
+++ doctor-service/src/main/java/com/rikkeiedu/doctorservice/controller/DoctorController.java
@@ -0,0 +1,31 @@
+package com.rikkeiedu.doctorservice.controller;
+
+import com.rikkeiedu.doctorservice.dto.DoctorDTO;
+import com.rikkeiedu.doctorservice.entity.Doctor;
+import com.rikkeiedu.doctorservice.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @GetMapping
+    public ResponseEntity<List<DoctorDTO>> getAllDoctors() {
+        List<DoctorDTO> doctors = doctorRepository.findAllProjected();
+        return new ResponseEntity<>(doctors, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor doctor) {
+        Doctor savedDoctor = doctorRepository.save(doctor);
+        return new ResponseEntity<>(savedDoctor, HttpStatus.CREATED);
+    }
+}
